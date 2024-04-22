e4c46f64eeabb548cce72e2a56e7d5965211d9c6
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-04-21 20:04:25 -0600

    Created a keyboard and logic for the words.

---
+++ src/components/HangmanWord.tsx
@@ -0,0 +1,34 @@
+//props object
+type Props = {
+  word: string;
+  usedLetters: string[];
+  showWord: boolean;
+};
+
+function HangmanWord({ word, usedLetters, showWord }: Props) {
+  return (
+    <div className="mt-5 text-3xl md:text-5xl flex items-center justify-center gap-2 font-bold">
+      {word.split("").map((letter: string, index: number) => (
+        <div
+          key={index}
+          className="w-8 md:w-12 border-b-4 border-sky-400 flex items-center justify-center"
+        >
+          <span
+            className={`${
+              usedLetters?.includes(letter) || showWord
+                ? "visible"
+                : "invisible"
+            } ${
+              !usedLetters?.includes(letter) && showWord
+                ? "text-red-600"
+                : "text-lime-300"
+            }`}
+          >
+            {letter.toUpperCase()}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default HangmanWord;
